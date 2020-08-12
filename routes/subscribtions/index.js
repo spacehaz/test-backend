20767284047b278c035c556db2c404b7913da564
spacehaz <spacehaz@gmail.com>
2020-08-13 02:48:24 +0300

    added fix

--- routes/subscribtions/index.js
+++ routes/subscribtions/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/:id', function (req, res) {
-  const { id } = req.params
+router.post('/', function (req, res) {
+  const { id } = req.body
   if (!id) {
-    return res.json({ success: false, error: 'id needed as param' })
+    return res.json({ success: false, error: 'id needed as body param' })
   }
   res.json({ success: true })
 })
